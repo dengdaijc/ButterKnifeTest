daae397f28dcf6549f1924366297b3c07ac5ba67
baiyu <fenghuo_baiyu@163.com>
2016-10-19 11:02:03 +0800

    Initial commit

---
+++ app/src/main/java/com/by/butterknifetest/MainActivity.java
@@ -0,0 +1,38 @@
+package com.by.butterknifetest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @BindView(R.id.textView)
+    TextView textView;
+    @BindView(R.id.button)
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @OnClick({R.id.textView, R.id.button})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.textView:
+                break;
+            case R.id.button:
+                break;
+        }
+    }
+}
